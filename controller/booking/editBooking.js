ddf20e06687e08072224204549ef41281e6fcb43
Dong Tan Huy <juliandong@Julians-MacBook-Pro.local>
2018-04-05 22:57:02 +0700

    handle error edit session

--- controller/booking/editBooking.js
+++ controller/booking/editBooking.js
@@ -4,7 +4,75 @@ const config = require('config')
 const Booking = require('./../../model/BookingSession')
 
 let editBooking = (req, res) => {
+  let id = req.params.id
+  let token = utils.getTokenFromHeaders(req.headers)
+  let updatedData = {
+    title: req.body.title,
+    date: req.body.date,
+    startAt: req.body.startAt,
+    endAt: req.body.endAt,
+    numberOfParticipants: req.body.numberOfParticipants,
+    room: req.body.room
 
+  }
+  if (!token) {
+    res.status(401)
+    res.json({
+      success: false,
+      message: 'Unauthorized'
+    })
+  } else if (!updatedData.title ||
+              !updatedData.date ||
+              !updatedData.startAt ||
+              !updatedData.endAt ||
+              !updatedData.numberOfParticipants ||
+              !updatedData.room) {
+    res.status(400)
+    res.json({
+      success: false,
+      message: 'Bad Request'
+    })
+  } else {
+    jwt.verify(token, config.get('passportSecret'), (err, result) => {
+      if (err) {
+        // Handle 500
+        if (err.name === 'JsonWebTokenError') {
+          res.status(500)
+          res.json({
+            success: false,
+            message: 'Server Error'
+          })
+        }
+        // Handle 400
+        if (err.name === 'TokenExpiredError') {
+          res.status(400)
+          res.json({
+            success: false,
+            message: 'Your token is invalid or expired'
+          })
+        }
+      } else {
+        Booking.findByIdAndUpdate(id, updatedData, (err, result) => {
+          if (err) {
+            res.status(404)
+            res.json({
+              success: false,
+              message: 'Bookings not found'
+            })
+          } else {
+            let currentUser = result._id
+            if (currentUser.toString !== result.organizer.toString()) {
+              res.status(403)
+              res.json({
+                success: false,
+                message: 'Permission denied'
+              })
+            }
+          }
+        })
+      }
+    })
+  }
 }
 
 module.exports = editBooking
--- test/booking/editBooking.spec.js
+++ test/booking/editBooking.spec.js
@@ -31,9 +31,16 @@ describe('Get specific booking session with given id', () => {
     'status': true,
     '__v': 0
   }
+  let newRoom1 = {
+    '_id': '5ab50e4018181c17b8fa1292',
+    'name': 'Test room',
+    'max_size': 15,
+    'status': true,
+    '__v': 0
+  }
   let roomID = newRoom._id
   let userID = user._id
-
+  let token = jwt.sign(user, config.get('passportSecret'))
   beforeEach(done => {
     Booking.remove((err) => {
       done(err)
@@ -43,6 +50,431 @@ describe('Get specific booking session with given id', () => {
   after(done => {
     Booking.remove((err) => {
       done(err)
+    })
+  })
+
+  it('It should return 401 if missing token', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      organizer: userID,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(401)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Unauthorized')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 404 if session not found', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id + 'sakfadhhfad')
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(404)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bookings not found')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 404 if missing id on URL', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(404)
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 403 if current user is not organizer', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: '5aaa02baf0fb4e0632f88aac',
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(403)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Permission denied')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 500 if invalid token', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT sdgsgf' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(500)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Server Error')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing title', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      // title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing date', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      // date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing start time', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      // startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing end time', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      // endAt: new Date() + 172800,
+      numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing number of Participants', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      // numberOfParticipants: 8,
+      room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
+    })
+  })
+
+  it('It should return 400 if missing room id', done => {
+    let newSession = new Booking({
+      title: 'Test session',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 86400,
+      numberOfParticipants: 10,
+      organizer: userID,
+      room: roomID
+    })
+    let updatedData = {
+      // title: 'Test session update',
+      date: new Date(),
+      startAt: new Date(),
+      endAt: new Date() + 172800,
+      numberOfParticipants: 8
+      // room: newRoom1._id
+    }
+
+    newSession.save((err, session) => {
+      if (err) {
+        console.log(err)
+      } else {
+        chai.request(server)
+          .put(API + session._id)
+          .set('token', 'JWT ' + token)
+          .send(updatedData)
+          .end((req, res) => {
+            res.should.have.status(400)
+            res.body.should.have.property('success')
+            res.body.success.should.be.eql(false)
+            res.body.should.have.property('message')
+            res.body.message.should.be.eql('Bad Request')
+            done()
+          })
+      }
     })
   })
 })
